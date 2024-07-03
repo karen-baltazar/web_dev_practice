9d177d21584ed46cea563c4f772133ca1cc59783
Karen Baltazar <a01707750@tec.mx>
2024-07-03 16:55:23 -0600

    [fitness-app] Create and style home page

--- fitness-app/tailwind.config.cjs
+++ fitness-app/tailwind.config.cjs
@@ -5,14 +5,14 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                "gray-20": "F8F4EB",
-                "gray-50": "EFE6E6",
-                "gray-100": "DFCCCC",
-                "gray-500": "5E0000",
+                "gray-20": "#F8F4EB",
+                "gray-50": "#EFE6E6",
+                "gray-100": "#DFCCCC",
+                "gray-500": "#5E0000",
                 "primary-100": "#FFE1E0",
-                "primary-200": "#FFA6A3",
-                "primary-300": "#FF6B66",
-                "secondary-400": "#FFCD58",
+                "primary-300": "#FFA6A3",
+                "primary-500": "#FF6B66",
+                "secondary-400": "#FFCD5B",
                 "secondary-500": "#FFC132",
             },
             backgroundImage: theme => ({ 
